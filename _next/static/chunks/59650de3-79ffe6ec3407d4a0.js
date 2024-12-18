af55b17c7ea97f9485b4cefe159637938c663e3c
Mael C. / Malek <74120418+MalekCrapule@users.noreply.github.com>
2024-12-18 15:39:51 +0100

    Updates

---
+++ _next/static/chunks/59650de3-79ffe6ec3407d4a0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[150],{2950:(t,e,n)=>{n.d(e,{AJ$:()=>r});var o=n(3435);function r(t){return(0,o.k5)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"polygon",attr:{points:"5 3 19 12 5 21 5 3"},child:[]}]})(t)}}}]);
